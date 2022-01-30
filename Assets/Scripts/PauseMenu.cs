748b9249e8cd77e67a222244e442e4a35bc20359
ryk0-lab <ricardpelaezc@gmail.com>
2022-01-30 03:21:40 +0100

    pause menu

--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -7,6 +7,8 @@ public class PauseMenu : MonoBehaviour
     public CameraController CameraController;
     public CanvasGroup OptionCanvasGroup;
     private CanvasGroup _canvasGroup;
+    public GameObject GlobalVolume;
+    public bool _escDisabled;
     private bool _show;
     private void Start()
     {
@@ -20,15 +22,16 @@ public class PauseMenu : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Escape))
         {
-            if (!_show)
+            if (!_escDisabled)
             {
-                Debug.Log("Show");
-                Show();
-            }
-            else
-            {
-                Debug.Log("Hide");
-                Hide();
+                if (!_show)
+                {
+                    Show();
+                }
+                else
+                {
+                    Hide();
+                }
             }
         }
     }
@@ -41,6 +44,7 @@ public class PauseMenu : MonoBehaviour
         _canvasGroup.interactable = false;
         _canvasGroup.blocksRaycasts = false;
         _show = false;
+        GlobalVolume.SetActive(false);
     }
     public void Show() 
     {
@@ -51,6 +55,7 @@ public class PauseMenu : MonoBehaviour
         _canvasGroup.interactable = true;
         _canvasGroup.blocksRaycasts = true;
         _show = true;
+        GlobalVolume.SetActive(true);
     }
     public void ShowOption()
     {
@@ -61,5 +66,18 @@ public class PauseMenu : MonoBehaviour
         OptionCanvasGroup.alpha = 1;
         OptionCanvasGroup.interactable = true;
         OptionCanvasGroup.blocksRaycasts = true;
+        _escDisabled = true;
+    }
+
+    public void HideOption()
+    {
+        _canvasGroup.alpha = 1;
+        _canvasGroup.interactable = true;
+        _canvasGroup.blocksRaycasts = true;
+        _show = true;
+        OptionCanvasGroup.alpha = 0;
+        OptionCanvasGroup.interactable = false;
+        OptionCanvasGroup.blocksRaycasts = false;
+        _escDisabled = false;
     }
 }
