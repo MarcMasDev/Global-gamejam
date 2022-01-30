eb82d6f39b68320dea881b46c2299ff304655d41
ryk0-lab <ricardpelaezc@gmail.com>
2022-01-30 02:57:51 +0100

    pause menu doing

--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -5,16 +5,31 @@ using UnityEngine;
 public class PauseMenu : MonoBehaviour
 {
     public CameraController CameraController;
+    public CanvasGroup OptionCanvasGroup;
     private CanvasGroup _canvasGroup;
+    private bool _show;
     private void Start()
     {
         _canvasGroup = GetComponent<CanvasGroup>();
+    }
+    public void Exit()
+    {
+        Application.Quit();
     }
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.Escape))
         {
-            Show();
+            if (!_show)
+            {
+                Debug.Log("Show");
+                Show();
+            }
+            else
+            {
+                Debug.Log("Hide");
+                Hide();
+            }
         }
     }
     public void Hide() 
@@ -25,6 +40,7 @@ public class PauseMenu : MonoBehaviour
         _canvasGroup.alpha = 0;
         _canvasGroup.interactable = false;
         _canvasGroup.blocksRaycasts = false;
+        _show = false;
     }
     public void Show() 
     {
@@ -34,5 +50,16 @@ public class PauseMenu : MonoBehaviour
         _canvasGroup.alpha = 1;
         _canvasGroup.interactable = true;
         _canvasGroup.blocksRaycasts = true;
+        _show = true;
+    }
+    public void ShowOption()
+    {
+        _canvasGroup.alpha = 0;
+        _canvasGroup.interactable = false;
+        _canvasGroup.blocksRaycasts = false;
+        _show = false;
+        OptionCanvasGroup.alpha = 1;
+        OptionCanvasGroup.interactable = true;
+        OptionCanvasGroup.blocksRaycasts = true;
     }
 }
