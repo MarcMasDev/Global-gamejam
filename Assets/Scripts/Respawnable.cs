66290e67bd4aa42863f4a98be1569c73a37b1eda
AinoaTA <atabares@edu.tecnocampus.cat>
2022-01-30 11:47:10 +0100

    level diana/basket

--- Assets/Scripts/Respawnable.cs
+++ Assets/Scripts/Respawnable.cs
@@ -6,33 +6,25 @@ public class Respawnable : MonoBehaviour
 {
     protected Vector3 initialPosition;
     protected Quaternion initialRotation;
-
-    private Rigidbody rb;
     private void Start()
     {
-        rb = GetComponent<Rigidbody>();
+      
+        SetPos();
+    }
+    public void SetPos()
+    {
         initialPosition = transform.position;
         initialRotation = transform.rotation;
     }
 
-    private void Update()
-    {
-        print(rb.velocity.magnitude);
-        
-    }
 
     private void OnTriggerEnter(Collider other)
     {
+       
         if (other.tag == "FallZone")
         {
             Respawn();
         }
-        if (other.CompareTag("Wall")&& (rb.velocity.magnitude < 1))
-        {
-            Respawn();
-        }
-
-
     }
 
     protected void Respawn()
